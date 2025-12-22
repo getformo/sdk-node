9b3dd7befee4090799f3042bb4620a50d5ee9844
Htet Shwe <htetshwewinthan@gmail.com>
2025-12-22 10:51:53 +0700

    chore: Update pnpm dependencies and add index.ts file

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Formo Analytics Node.js SDK
+ *
+ * @packageDocumentation
+ */
+
+export {
+  FormoAnalytics,
+  TrackAPIEvent,
+  IdentifyAPIEvent,
+  AnalyticsOptions,
+  ValidationError,
+} from "./FormoAnalytics";
